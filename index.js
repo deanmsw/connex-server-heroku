93916c92d86c0af6f2937f8cb7c6a50c64795f82
deanmsw <dean@mysuperiorwebsite.com>
2022-06-08 08:17:56 +1000

    cors removed

--- index.js
+++ index.js
@@ -1,11 +1,8 @@
 import express from "express";
-const cors = require('cors')
 
 const PORT = process.env.PORT || 5000;
 
 const app = express();
-
-app.use(cors())
 
 app.get("/api", (req, res) => {
   res.json({ title: "Hello from Dean Scotthornes server!" });
