2dae4e11f5de77f12b1e815f92e45269e781e035
deanmsw <dean@mysuperiorwebsite.com>
2022-06-08 08:45:48 +1000

    cors package added

--- index.js
+++ index.js
@@ -1,8 +1,10 @@
 import express from "express";
+import cors from 'cors';
 
 const PORT = process.env.PORT || 5000;
 
 const app = express();
+app.use(cors());
 
 app.get("/api", (req, res) => {
   res.json({ title: "Hello from Dean Scotthornes server!" });
